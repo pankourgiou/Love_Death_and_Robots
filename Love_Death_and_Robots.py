da918df2dd97ed9e1c3f7e31e6d8519aa403a000
pankourgiou <60981745+pankourgiou@users.noreply.github.com>
2023-04-27 05:45:14 +0300

    Create Love_Death_and_Robots.py

---
+++ Love_Death_and_Robots.py
@@ -0,0 +1,38 @@
+#You will live again! Your theories are alive. It's in your hand to live life again. You should watch things the way you used to watch them. This means to live again. -Marcus Aurelius.
+#Cultivate your Memory and you will live a thousand lives. - Umberto Eco
+#We must reinvent faith but the same time we must be rediscovering the meanings of life the reason we do science and the reasons we believe in God the same time. 
+#Seems too many things have been written and said about love, death and robots. I start talking like this because I know part of a robot engine has soul as if it's maker has done something with much care.
+#We will try here to remake past in a future tense. Or will we remake future in a past tense or maybe we should do both in the present. I think the later is better.
+#Right now I listen to one of my favourite albums of all time Liquid Tension Experiment 3. Their sound is extra cool, it's more chill than the previous two super cool albums but the same time their fusion style jamming is more open minded and their ideas are clear. I love these guys.
+#Umberto Eco's talk on laughter and the meaning of life will always be my guide in life, since sience is what I love. Lights!!! More Lights!!!
+#In the first place I couldn't understand many thoughts on Umberto Eco's writing...I still don't yet I now see he tried to warn us all on something...
+#Let's think of this simple yet complex idea, if you were the best wouldn't you like experimenting with society around you? wouldn't you like playing games and having fun?
+#I think you would so if God and religion dictate you to think different wise wouldn't this mean they malfunction??
+#This idea should be central....we leave religion without criticism so that religion won't criticise us...but we can't afford this church uber alles idea, we shouldn't afford this idea it's a cheap trick that costs more than we think.
+#I am saying this not faint hearted...right now writing this script I know this very idea might be troubling me in the future.
+#Jesus Christ and his teachings on love are very importand...very importand since love there should be. Yet we should evolve from this point to another and we can do it since the energy to do so exists.
+#It's 4 o'clock at night and in a couple of hours we will see the sun rising, a new day will start, a complex day, yet our target should be simple... Have fun enjoy and celebrate life!!!
+#Matterials are limited time is unlimited so everything will be repeated...We perceive Jesus Christ in a limited fashion. Jesus is more than one person he is multiple persons and one the same time discovering life and the meanings of it throughout time and space using various forms.
+#Not a single idea or thesis should be thought a garbage for this system of thinking.
+#Example: All people carry God inside them yet it's society's mission to remind this central idea of love to every person constantly.
+#Leading the example one step further: Wasn't dirac or Euler good humans? didn't they have love for what they did? aren't their teachings divine? I think they are.
+#God say those who have two shirts they should give one...God made more mistakes than we think he did for he was human, but why are we throwing a rock on him? We never have sinned?
+#There is no way to understand God's plan if we never use our hands to make a plan on our own, and this is a difficult teaching.
+#So first we make plans then we see God's plan more clearly. we understand! 
+#I want to talk on this little cute hero it's my sister. My sister tries hard and she is successful!!! She is successful because she helps without been seen, without been heard.
+#She helps and she loves.
+#This is my actual subject right now!!! My sister's games and ideas. Koran prohibits the visualisation of imaginative animals. Pokemons are a card game and they are prohibited by islam like parts of the "pro-islamic montrocity".
+#My sister saves lifes through peace love and understanding and those game like pokemon formed her life-style. I am not trying to challenge Muslims. I am trying to enforce people to a broader love centered life/game which will bring peace eventually. 
+# I try to speak on world peace through a Christian life style, but to do so we need to broaden our point of view!!!Was my sister wrong? I am pretty sure she wasn't wrong, and I'm pretty sure there is a secret power behind this game.
+# This is the theory, wisdom through action!!! Ultimate philosophical idea here is eternal life through love and peace. Those who believe in war idea have no chance no thesis on paradise.
+# Paradise is a state of mind, it's this state of mind where your body-mind chemicals change move and mix so that we reach a harmony...paradise should smell like harmony.
+# In order to reach this state of mind someone needs to walk the road of spirituality, and this road is the road from the top and above.
+# My sister also likes wolves apart from magic tricks and planning. She likes wolves as a symbol of being fair...and she is fair. My emotions while she used to play this pokemon game were good, since I could watch her playing and I knew she was safe.
+#So one more point that has to do with playing games(like pokemon) and philosophising concept is that they keep you safe...it's not like any extreme sport or something, it's just you and the video game inside four walls trying to win the level.
+#I see those pictures realizing there is a catch. People even managed to create, a working office environment so that we play office game. That's not really cool. It has a design or something, but it's  not a very advanced idea.
+#Humans like clean solutions, so this might be the idea we should ceize. To want something with all our emotions is the method to acquire it. What we want is action! 
+#The Valis system talks some serious ideas, yet some of them are not crystal clear christian orthodox, the teachings are good but why do gnostinc christians think of God as an evil entity???
+#This shouldn't be real, is this a malfunction a secret backdoor on christianity?
+#The theory is this: planning means change, planning means to differentiate yourself form one point to another...THIS IS THE HUMAN TOUCH AND HOW IT WORKS.
+#If everything , if every plan is different this means everything is about action!!! There is no need for theory. Evil is theoretical much, and there is theory tons of it!!!
+#But and this is a big but, evil seems to be like an animal...we can try soften it's angles...make it soft...a new day comes, the sun will shine we will have fun, and I will change the album change the track watch more pixel-art, thinking I'm a rock believer.
